dc5350c2d0439186f3bd0d0262b91f99d05c4b68
Arman Harutyunyan <armanharut98@gmail.com>
2025-02-11 16:49:17 +0400

    save pr body to file and read from it

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import markdown_to_json
 with open("./pr_body", "r") as file:
     text = file.read()
     d = markdown_to_json.dictify(text)
-    print(d["Issue Title"])
     print(d["Business Justification"])
     print(d["What testing did you do? Provide a link to results."])
     print(d["Does this have model changes?"])
