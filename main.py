4985f6044170d564443f9c86f69621e1c0e2217e
Arman Harutyunyan <armanharut98@gmail.com>
2025-02-11 18:44:37 +0400

    save pr body to file and read from it

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ with open("./pr_body", "r") as file:
     text = file.read()
     d = markdown_to_json.dictify(text)
     for key, value in d.items():
-        print(d[key])
+        print(d[key], len(d[key]))
         if type(d[key]) is list:
             continue
-        if d[key] is None:
+        if d[key] == "":
             sys.exit("Not all RAP fields have been filled in.")
 
     print("All fields are filled!")
