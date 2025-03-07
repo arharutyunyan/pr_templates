40c65ef846b3bd618639b4aa8367b33ff8302c47
Arman Harutyunyan <armanharut98@gmail.com>
2025-03-07 21:46:42 +0400

    minor fix

--- main.py
+++ main.py
@@ -74,4 +74,4 @@ issue = jira.create_issue(fields=ticket_fields)
 bug_ticket_key = d["Bug Ticket Key"]
 
 # Create Issue Link
-jira.create_issue_link(type, issue.key, bug_ticket_key)
+jira.create_issue_link("relates to", issue.key, bug_ticket_key)
